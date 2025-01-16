a9579e48fda636ff32b9cf6ef81547601916ff83
Sebb <39489459+sebb01@users.noreply.github.com>
2025-01-16 15:21:42 +0100

    Keep /build directory, cleaner make.bat

--- make.bat
+++ make.bat
@@ -1,5 +1,8 @@
 set LCC=".\gbdk\bin\lcc"
+set FLAGS=-Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -Wl-yt1 -Wl-yo4 -Wl-ya0 -msm83:gb
 
-%LCC% -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -Wl-yt1 -Wl-yo4 -Wl-ya0 -msm83:gb -o .\build\SnakeBoy.gb main.c
+%LCC% %FLAGS% -o .\build\SnakeBoy.gb main.c
+
+REM Clean up build files
 cd .\build
 del *.o *.lst *.ihx *.map *.noi *.asm *.sym
